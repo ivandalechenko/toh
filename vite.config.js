fd51a82b8c921c8aeb1d170c23504a680d600564
ivandalechenko <ivandalechenko@gmail.com>
2025-01-04 00:34:52 +0200

    added some

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
   resolve: {
     alias: {
       buffer: 'buffer',
     },
+  },
+  define: {
+    global: 'globalThis',
   },
 });
